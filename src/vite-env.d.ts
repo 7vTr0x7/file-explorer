51fb57fdd38722edf45f3ebd3131713f9ba9c846
7vTr0x7 <7vtrox7@gmail.com>
2024-04-09 17:59:55 +0530

    ExplorerType

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,23 @@
 /// <reference types="vite/client" />
+
+type ExplorerType = {
+  id: string;
+  name: string;
+  isFolder: boolean;
+  items: {
+    id: string;
+    name: string;
+    isFolder: boolean;
+    items: {
+      id: string;
+      name: string;
+      isFolder: boolean;
+      items: {
+        id: string;
+        name: string;
+        isFolder: boolean;
+        items: never[];
+      }[];
+    }[];
+  }[];
+};
